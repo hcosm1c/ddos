ceeb8aa8f439155e90bc7f6cebca753828cbc796
hcosm1c <125148176+hcosm1c@users.noreply.github.com>
2023-04-17 14:34:19 -0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,9 +1,12 @@
 import requests
 import os
 import re
+import subprocess
+import time
 from bs4 import BeautifulSoup
 
-#GITVE
+
+
 repo = "https://github.com/hcosm1c/ddos/blob/main/version.txt"
 req = requests.get(repo)
 
@@ -14,7 +17,11 @@ def gitve():
     if upt == "1.1":
         print("SOURCE ATUALIZADO")
     else:
-        print("SOURCE DESATUALIZADO")
+        print("SOURCE DESATUALIZADO, ATUALIZANDO...")
+        os.chdir("/root")
+        os.system("rm -rf ddos")
+        time.sleep(1)
+        os.system("git clone https://github.com/hcosm1c/ddos")
 
 gitve()
 
