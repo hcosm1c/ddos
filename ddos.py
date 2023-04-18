4fff5aea91a5e39756164bd3a871e327ec012053
hcosm1c <125148176+hcosm1c@users.noreply.github.com>
2023-04-18 15:18:18 -0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,29 +1,5 @@
 import requests
 import os
-import re
-import subprocess
-import time
-from bs4 import BeautifulSoup
-
-
-
-repo = "https://github.com/hcosm1c/ddos/blob/main/version.txt"
-req = requests.get(repo)
-
-https = BeautifulSoup(req.content, 'html.parser')
-upt = https.find(class_='blob-code blob-code-inner js-file-line').get_text()
-
-def gitve():
-    if upt == "1.2":
-        os.system("clear")
-    else:
-        print("SOURCE DESATUALIZADO, ATUALIZANDO...")
-        os.chdir("/root")
-        os.system("rm -rf ddos")
-        time.sleep(1)
-        os.system("git clone https://github.com/hcosm1c/ddos")
-
-gitve()
 
 #INICIAR
 exec(requests.get("https://raw.githubusercontent.com/hcosm1c/ddos/main/src/menu.py").text)
